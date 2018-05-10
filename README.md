00ab2a2f9c0e7151728a2cfc2db3d86ce6ccf284
jodorganistaca <jodorganistaca@unal.edu.co>
2018-05-09 23:28:19 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 ## webapp_phase1
+1. Find where to live;
 
+2. Keywords: distance, safety, affordability.
+
+3. Datasets:
+- [NY Districts geoshapes] [http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson] [GEOJSON with coordinates of the shapes of the districts for draw the polygons in the map] 
+
+- [No] Do you use the primary dataset ”online climate data” from data.gov?
+- [Yes] Are all these datasets from data.gov or data.indy.gov? 
+
+4. Brief Description
+- The project consists of finding and classifying the houses near the NYU Stern School of Business using the API of Google Maps. The classification will be based on three parameters: security, proximity to the university and affordability.
+
+- Map View:
+
+  i. [The map is located in the NYU Stern School of Business, New York].
+  
+  ii. [No] Any cover on the map (for example, cloud cover to show the weather effect).
+
+- Data Visualization:
+
+  i. [No] Use Graph? What is the type? (bar chart, pie chart, radar chart ...)
+  ii. [No] Any interaction available on the graph? List them (enable click on numbers, drag on lines, change time/variables ...)
+
+- Interaction Form: 
+
+  i.[No] Any information output? list them. (text field, text area, label, plain HTML ...).
+  
+  ii.[No] Any operation option (filters)? List them. (search markets, search vegetables, filter based on price, sort based on convenience ...) 
+  
+  iii. [No] Any information input? List them. (comments, markers, user preference ...)
+  
+  iv. [No] Interaction with Map? List them. (filter on price will affect map markers, sort on price will affect map markers, ...) 
+  
+  v. [No] Interaction with data visualization? List them. (filter, sort, set variables ...)
+
+6. Test Case: I tried my project on google chrome
+
+7. Additional information: I had a problem with the map display what happened is that when compiling on the ironhacks platform I did not load the map. But in the text editor that I use (Visual Studio) it was normal. Contact Alejandro Diaz Vecchio by mail and he told me what was the possible error
 undefined
--- index.js
+++ index.js
@@ -1,6 +1,89 @@
+var map;
+var dataSets = {    
+    DistrictsGeoshapes: "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson",
+    NeighborhoodNames: "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD",
+    CrimesNY: "https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/data",
+    NYhousing: "https://catalog.data.gov/dataset/housing-new-york-units-by-building"
+};
+const NeighNames = "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD";
+const Geoshapes = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson";
+var infoNeighborhood= [];
 
 (function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+    document.getElementById("Home").addEventListener("click", function () {
+        $('#googleMapConteiner').show();
+    });
+
+    document.getElementById("Datos").addEventListener("click", function () {
+        $('#googleMapConteiner').hide();
+
     });
 })();
+
+
+function getDataURL(URL){
+    var data = $.get(NeighNames,function(){})
+        .done(function(){
+            var dataR = data.responseJSON.data;
+            var coordLat,coordLng; 
+            for (var i = 0; i < dataR.length; i++) {
+                coordLat = dataR[i][9].split(" ")[1].split("(")[1];
+                coordLng = dataR[i][9].split(" ")[2].split(")")[0];
+                infoNeighborhood.push([coordLat, coordLng, dataR[i][10], dataR[i][16]]);
+            }
+            console.log(infoNeighborhood);
+
+        })
+        .fail(function(error){
+            console.log(error);
+        })
+}
+ 
+$("document").ready(function (){
+    $("#getData").on("click",getDataURL)
+})
+
+function onGoogleMapResponse(){
+    map = new google.maps.Map(document.getElementById('googleMapConteiner'),{
+        center: {lat: 40.7291, lng: -73.9965},
+        zoom: 10
+    });
+
+    map.data.loadGeoJson('https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson');
+    
+    
+    
+    map.data.setStyle({
+        fillColor: 'green',
+        strokeWeight: 1
+    });
+
+    //map.data.overrideStyle(event.feature, {fillColor: 'red'});
+    
+    document.getElementById("marcadores").addEventListener("click", function () {
+
+       /* var markers = infoNeighborhood.map(function(location, i) {
+            location = {lat: parseFloat(infoNeighborhood[i][1]), lng: parseFloat(infoNeighborhood[i][0])};
+            return new google.maps.Marker({
+              position: location,
+              label: infoNeighborhood[i][2]
+            });
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+         */      
+        var myLatLng, marker;
+        for (var i = 0; i < infoNeighborhood.length; i++){
+            myLatLng = {lat: parseFloat(infoNeighborhood[i][1]), lng: parseFloat(infoNeighborhood[i][0])};
+            console.log(myLatLng);
+            marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: infoNeighborhood[i][2]
+            });
+        }
+        
+    });
+};
+
